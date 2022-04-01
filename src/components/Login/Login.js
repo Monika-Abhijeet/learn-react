81f3777efe2316a60fd2e090f4d045f952e83718
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-04-01 16:33:35 +0530

    basic structure

---
+++ src/components/Login/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Login.css";
+function Login() {
+  return (
+    <div className="login-container">
+      <h1>Login Screen</h1>
+      {/* <form className="form form-container">
+        <label htmlFor="userId" className="form-label">
+          Userid
+        </label>
+        <input
+          type="text"
+          name="userId"
+          id="userId"
+          className="form-control"
+          placeholder="enter your user id"
+        />
+        <label>password</label>
+
+        <input type="password" name="password"></input>
+
+        <button className="btn btn-danger">Login</button>
+      </form> */}
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,6 @@
+import "./Sidebar.css";
+function Sidebar() {
+  return <div className="sidebar-container">sidebar</div>;
+}
+
+export default Sidebar;
